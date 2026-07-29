e880d932bef54584e3ba50f58f4bee90f513dc6d
techfusion3 <faijanahamed11@gmail.com>
2026-07-29 17:31:14 +0530

    refactor: move Socket.IO logic to separate module

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,4 +8,43 @@ const initializeSocket = (server) => {
       methods: ["GET", "POST"],
     },
   });
+  const onlineUsers = new Map();
+
+  io.on("connection", (socket) => {
+    console.log("User Connected:", socket.id);
+
+    socket.on("join", (userId) => {
+      onlineUsers.set(userId, socket.id);
+
+      io.emit("online_users", [...onlineUsers.keys()]);
+    });
+
+    // send message
+    socket.on("send_message", async (data) => {
+      try {
+        const message = await Message.create({
+          sender: data.sender,
+          text: data.text,
+        });
+    
+        io.emit("receive_message", message);
+      } catch (error) {
+        console.error("Message Error:", error);
+      }
+    });
+    // disconnect
+    socket.on("disconnect", () => {
+      console.log("User Disconnected:", socket.id);
+    
+      for (const [userId, socketId] of onlineUsers.entries()) {
+        if (socketId === socket.id) {
+          onlineUsers.delete(userId);
+          break;
+        }
+      }
+    
+      io.emit("online_users", [...onlineUsers.keys()]);
+    });
+
+  });
 };
